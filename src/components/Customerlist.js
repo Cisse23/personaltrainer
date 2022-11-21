34ac3baef1c5da85d83bb35d1b3f140756f58617
Cisse Baker <baker.cisse@gmail.com>
2022-11-21 14:53:49 +0200

    ag-grid asennettu, customerlist ja getCustomers tehty

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -1,29 +1,55 @@
-import React from "react"
+import React, { useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
-import 'ag-grid-community/dist/styles/ag-grid.css'
-import 'ag-grid-community/dist/styles/ag-theme-material.css'
-;
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 export default function Customerlist(){
     const [customers, setCustomers] = React.useState([]);
-    
+    const link = 'https://customerrest.herokuapp.com/api/customers'
     const columns = [
-        {field: "firstname"}, 
-        {field: "lastname"},
-        {field: "streetaddress"},
-        {field: "postcode"},
-        {field: "city"},
-        {field: "email"}]
+        {field: "firstname", sortable: true, filter: true}, 
+        {field: "lastname", sortable: true, filter: true},
+        {field: "streetaddress", sortable: true, filter: true},
+        {field: "postcode", sortable: true, filter: true},
+        {field: "city", sortable: true, filter: true},
+        {field: "email", sortable: true, filter: true}
+    ]
+
+
+    const getCustomers = (() => {
+        fetch('https://customerrest.herokuapp.com/api/customers')
+        .then(response => {
+            if(response.ok)
+                return response.json();
+            else
+                alert('Error when getting customers ' + response.statusText);
+        })
+        .then(data => setCustomers(data.content))
+        .catch(err => console.error)
+    }, [] );
+
+    useEffect(() => {
+        fetch(link)
+        .then(response => {
+            if(response.ok)
+                return response.json();
+            else
+                alert('Error when getting customers' + response.statusText)
+        })
+        .then(data => setCustomers(data.content))
+        .catch(err => console.error)
+    }, []);
+
+
 
     return(
         <div className="ag-theme-material"
-        style={{height: '700px', width: '70%', margin: 'auto'}} >
+            style={{height: '900px', width: '90%', margin: 'auto'}} 
+        >
             <AgGridReact
-            columnDefs={columns}
-            rowData={customers}
-            >
-
-            </AgGridReact>
+                columnDefs={columns}
+                rowData={customers}
+            />
         </div>
     );
 }
