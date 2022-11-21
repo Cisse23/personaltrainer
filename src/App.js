7aab1bc213acfe9e3756f60e09c391f7377b1717
Cisse Baker <baker.cisse@gmail.com>
2022-11-21 12:55:06 +0200

    mui asennettu ja tabit tehty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import BasicTabs from './components/Tabs'
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar>
+        <Toolbar>
+          <Typography variant='h4'>Personal Trainer</Typography>
+
+        </Toolbar>
+      </AppBar>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <BasicTabs />
+
     </div>
   );
 }
 
 export default App;
+
+
+/*
+Osa 1
+Luo React sovellus, jossa on omat listasivut asiakkaille (customer) 
+ja harjoituksille (training). Luo myös navigaatio, jolla sivujen välillä voi siirtyä.
+Listasivujen minimivaatimukset:
+•	Järjestely (sorting)
+•	Tietojen haku/suodatus eri sarakkeiden perusteella
+•	Näytä myös asiakkaan nimi harjoitus -listasivulla
+•	Muotoile päivämäärä taulkossa esim. mutoon pp.kk.vvvv hh:mm
+
+*/
+
+
+/*
+
+*/
