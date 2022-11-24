4736976f7a19a56b221fdeaf171a185763c61026
Cisse Baker <baker.cisse@gmail.com>
2022-11-24 16:25:25 +0200

    päivämäärä formatoitu, asiakkaan nimi ei toimi vielä

--- src/components/Traininglist.js
+++ src/components/Traininglist.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { AgGridReact } from "ag-grid-react";
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
@@ -8,14 +8,34 @@ export default function Traininglist(){
     
     const [trainings, setTrainings] = React.useState([]);
     const link = 'https://customerrest.herokuapp.com/api/trainings'
+    
+    //https://ag-grid.com/react-data-grid/value-formatters/
+    const dateFormatter = (params) => {
+        return (
+            dayjs(params.data.date).format('DD.MM.YYYY HH.mm')
+        );
+        //return '£' + formatNumber(params.value);
+    };
+
+    const getCustomer = (params) => {
+        console.log(params.data.links[2].toString())
+        return params.data.links[2];
+    };
+    
     const columns = [
-        {field: "date", sortable: true, filter: true, valueFormatter: dateFormatter}, 
+        {field: "date", headerName: "ISO date"}, 
+        {field: "date", headerName: "Date formatted", valueFormatter: dateFormatter}, 
         //Muotoile päivämäärä taulkossa esim. mutoon pp.kk.vvvv hh:mm
         //dayjs().format("YYYY-MM-DD"); // 2021-05-26
-        {field: "duration", sortable: true, filter: true},
-        {field: "activity", sortable: true, filter: true},
-        {field: "customer", sortable: true, filter: true} //Näytä myös asiakkaan nimi harjoitus -listasivulla
+        {field: "duration"},
+        {field: "activity"},
+        {field: "customer", valueGetter: getCustomer} //Näytä myös asiakkaan nimi harjoitus -listasivulla
     ]
+
+    const defaultColDef = useMemo( () => ({
+        sortable: true,
+        filter: true
+    }), [] );
 
     const getTrainings = (() => {
         fetch(link)
@@ -41,12 +61,15 @@ export default function Traininglist(){
         .catch(err => console.error)
     }, [] );
 
+
+
     return(
         <div className="ag-theme-material"
             style={{height: '900px', width: '90%', margin: 'auto'}} 
         >
             <AgGridReact
                 columnDefs={columns}
+                defaultColDef={defaultColDef}
                 rowData={trainings}
             />
         </div>
